cac5ec122ec86513bb6f43c9f35d57fd37b53efc
Giel Berkers <info@gielberkers.com>
2010-08-05 12:29:00 +0200

    initial commit

---
+++ assets/custom.js
@@ -0,0 +1,8 @@
+/*
+	In this script you can use the following parameters:
+	
+	var driver = 'publish';		= The current page in the backend (like 'publish' or 'blueprintspages')
+	var action = 'new';			= The current action (like 'new' or 'edit')
+	var section= 'banners';		= The name of the section
+	var id_entry = false;		= The ID of the entry that is edited
+*/
---
+++ extension.driver.php
@@ -0,0 +1,61 @@
+<?php
+	Class extension_backend_add_script extends Extension
+	{
+		/**
+		* About this extension:
+		*/
+		public function about()
+		{
+			return array(
+				'name' => 'Backend add script',
+				'version' => '1.0',
+				'release-date' => '2010-08-05',
+				'author' => array(
+					'name' => 'Giel Berkers',
+					'website' => 'http://www.gielberkers.com',
+					'email' => 'info@gielberkers.com'),
+				'description' => 'Adds a hook to the backend so you can easily make JavaScript adjustments on certain pages'
+			);
+		}
+		
+		/**
+		* Set the delegates
+		*/
+		public function getSubscribedDelegates()
+		{
+			return array(
+				array(
+					'page' => '/backend/',
+					'delegate' => 'InitaliseAdminPageHead',
+					'callback' => 'addScriptToHead'
+				)
+			);
+		}
+		
+		/**
+		 * Add script to the <head>-section of the admin area
+		 */
+		public function addScriptToHead($context)
+		{
+			// We have to put a lot of javascript here since there are missing some handles:
+			$javaScript = "\n";
+			$callback   = $context['parent']->getPageCallback();
+			
+			// Current callback and action:
+			$driver = '"'.$callback['driver'].'"';
+			$action = isset($callback['context']['page']) ? '"'.$callback['context']['page'].'"' : 'false';
+			$section= isset($callback['context']['section_handle']) ? '"'.$callback['context']['section_handle'].'"' : 'false';
+			$idEntry= isset($callback['context']['entry_id']) ? '"'.$callback['context']['entry_id'].'"' : 'false';
+			
+			$javaScript.= "var driver   = ".$driver.";\n";
+			$javaScript.= "var action   = ".$action.";\n";
+			$javaScript.= "var section  = ".$section.";\n";
+			$javaScript.= "var id_entry = ".$idEntry.";\n";
+			
+			$tag = new XMLElement('script', $javaScript, array('type'=>'text/javascript'));
+			$context['parent']->Page->addElementToHead($tag, 50);
+			$context['parent']->Page->addScriptToHead(URL.'/extensions/backend_add_script/assets/custom.js', 51, false);
+			$context['parent']->Page->addStylesheetToHead(URL.'/extensions/backend_add_script/assets/custom.css', 'screen', 201);
+		}	
+	}
+?>
